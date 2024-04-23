6e7c527c034ac24b7ff71efae1f4d4b8c95fd31a
pikat <bencheikhkaouther.n@gmail.com>
2024-04-23 10:37:47 +0200

    premier commit : ajout de mon projet java

---
+++ src/main/java/org/example/projet_ci/ProjetCiApplication.java
@@ -0,0 +1,13 @@
+package org.example.projet_ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projet_ci/ProjetCiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projet_ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
